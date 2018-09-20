a3d58b88cc127649e3261921b1c1d011e9006b89
chen <chenchengyi@bitflash.vip>
2018-09-20 13:47:17 +0800

    sdsdsdsds

--- bkc-model/src/main/java/cn/bitflash/controller/UserRelationController.java
+++ bkc-model/src/main/java/cn/bitflash/controller/UserRelationController.java
@@ -2,6 +2,7 @@ package cn.bitflash.controller;
 
 
 import cn.bitflash.entity.UserRelationEntity;
+import cn.bitflash.entity.UserRelationJoinAccountEntity;
 import cn.bitflash.exception.RRException;
 import cn.bitflash.service.UserRelationService;
 import com.alibaba.fastjson.JSONObject;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 /**
  * @author GAOYUGUO
  */
@@ -20,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserRelationController {
 
     @Autowired
-    private UserRelationService service;
+    private UserRelationService userRelationService;
 
     /**
      * selectById
@@ -29,7 +32,7 @@ public class UserRelationController {
      */
     @PostMapping("/inner/userRelation/selectById")
     public JSONObject selectById(@RequestParam("id") String id) {
-        UserRelationEntity entity = service.selectById(id);
+        UserRelationEntity entity = userRelationService.selectById(id);
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("uid", entity.getUid());
         jsonObject.put("invitation_code", entity.getInvitation_code());
@@ -53,7 +56,7 @@ public class UserRelationController {
         entity.setLft(json.getInteger("lft"));
         entity.setRgt(json.getInteger("rgt"));
         entity.setLayer(json.getInteger("layer"));
-        service.updateById(entity);
+        userRelationService.updateById(entity);
     }
 
     /**
@@ -70,7 +73,7 @@ public class UserRelationController {
         entity.setLft(json.getInteger("lft"));
         entity.setRgt(json.getInteger("rgt"));
         entity.setLayer(json.getInteger("layer"));
-        service.insert(entity);
+        userRelationService.insert(entity);
     }
 
     /**
@@ -81,7 +84,19 @@ public class UserRelationController {
     @PostMapping("/inner/userRelation/deleteById")
     @Transactional(propagation = Propagation.REQUIRED, rollbackFor = RRException.class)
     public void deleteById(@RequestParam("id") String id) {
-        service.deleteById(id);
+        userRelationService.deleteById(id);
+    }
+
+
+    @PostMapping("/inner/userRelation/insertTreeNode")
+    public void insertTreeNode(@RequestParam("f_uid") String f_uid,@RequestParam("c_uid") String c_uid,
+                               @RequestParam("invitation_code") String invitation_code){
+        userRelationService.insertTreeNode(f_uid,c_uid,invitation_code);
+    }
+
+    @PostMapping("/inner/userRelation/selectTreeNodes")
+    public List<UserRelationJoinAccountEntity> selectTreeNodes(@RequestParam("f_uid")String f_uid){
+        return userRelationService.selectTreeNodes(f_uid);
     }
 
 }
